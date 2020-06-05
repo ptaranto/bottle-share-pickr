405e2c9b2c34f7b91b6d28af0d843c818f40cae2
Pedro Taranto <github@pedrotaranto.com.br>
2020-06-05 18:11:03 +0100

    add redirect url to environment config file

--- bottle-share-pickr/apps/api/src/environments/environment.prod.ts
+++ bottle-share-pickr/apps/api/src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  untappdRedirectUrl:
+    'http://projects.pedrotaranto.com.br/bottle-share-pickr/auth'
 };
--- bottle-share-pickr/apps/api/src/environments/environment.ts
+++ bottle-share-pickr/apps/api/src/environments/environment.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: false
+  production: false,
+  untappdRedirectUrl: 'http://localhost:4200/auth'
 };
