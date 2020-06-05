e33c6fe82f282fe9c22e0ae2e7414fc7cb37343d
Pedro Taranto <github@pedrotaranto.com.br>
2020-06-05 22:05:43 +0100

    add untappd auth urls and endpoints to api-lib

--- bottle-share-pickr/apps/api/src/app/app.controller.ts
+++ bottle-share-pickr/apps/api/src/app/app.controller.ts
@@ -4,6 +4,10 @@ import { Controller, Get, Query, Redirect } from '@nestjs/common';
 import { AppService } from './app.service';
 import { ConfigService } from '@nestjs/config';
 import { environment as env } from '../environments/environment';
+import {
+  UNTAPPD_AUTHENTICATE_URL,
+  UNTAPPD_AUTHORIZE_URL
+} from '@bottle-share-pickr/api-interface';
 
 @Controller()
 export class AppController {
@@ -18,24 +22,24 @@ export class AppController {
   }
 
   @Get('redirectToUntappdAuth')
-  @Redirect('https://untappd.com/oauth/authenticate/')
+  @Redirect(UNTAPPD_AUTHENTICATE_URL)
   authenticateUntappdUser() {
     const untappdClientId = this.configService.get<string>('UNTAPPD_CLIENT_ID');
     const redirectUrl = env.untappdRedirectUrl;
     return {
-      url: `https://untappd.com/oauth/authenticate/?client_id=${untappdClientId}&response_type=code&redirect_url=${redirectUrl}`
+      url: `${UNTAPPD_AUTHENTICATE_URL}?client_id=${untappdClientId}&response_type=code&redirect_url=${redirectUrl}`
     };
   }
 
   @Get('retrieveUntappdToken')
-  @Redirect('')
+  @Redirect(UNTAPPD_AUTHORIZE_URL)
   async authorizeUntappdUser(@Query('code') code: string) {
     const redirectUrl = env.untappdRedirectUrl;
     const untappdClientId = this.configService.get<string>('UNTAPPD_CLIENT_ID');
     const untappdClientSecret = this.configService.get<string>(
       'UNTAPPD_CLIENT_SECRET'
     );
-    const url = `https://untappd.com/oauth/authorize/?client_id=${untappdClientId}&client_secret=${untappdClientSecret}&response_type=code&redirect_url=${redirectUrl}&code=${code}`;
+    const url = `${UNTAPPD_AUTHORIZE_URL}?client_id=${untappdClientId}&client_secret=${untappdClientSecret}&response_type=code&redirect_url=${redirectUrl}&code=${code}`;
     const tokenResponse: any = await axios.get(url);
 
     const token = tokenResponse.data.response.access_token;
--- bottle-share-pickr/libs/api-interface/src/lib/api-interface.ts
+++ bottle-share-pickr/libs/api-interface/src/lib/api-interface.ts
@@ -1,5 +1,17 @@
 export const API_URL = 'api';
 
+export const UNTAPPD_AUTHENTICATE_URL =
+  'https://untappd.com/oauth/authenticate/';
+export const UNTAPPD_AUTHORIZE_URL = 'https://untappd.com/oauth/authorize/';
+export const UNTAPPD_ROOT_URL = 'https://api.untappd.com/v4/';
+
+// Untappd endpoints
+export const USER_INFO = 'user/info';
+export const USER_WISHLIST = 'user/wishlist';
+export const USER_FRIENDS = 'user/friends';
+export const BEER_INFO = 'beer/info';
+export const SEARCH_BEER = 'search/beer';
+
 export interface DefaultResponse {
   message: string;
 }
